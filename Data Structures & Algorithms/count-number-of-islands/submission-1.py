f37ee59bc5e8d09a5671255f387f8b3c5ed695cd
Aditya <87596501+AdityaEXP@users.noreply.github.com>
2026-06-09 15:39:58 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        approch iterate grid and run dfs on every 1 i find and in dfs i wil change every 1 to 0 to marked them as explored!
+        """
+        totalIsland = 0
+        ROWS, COLUMNS = len(grid), len(grid[0])
+
+        def dfs(r, c, seen):
+            if (
+                min(r, c) < 0 or 
+                r >= ROWS or
+                c >= COLUMNS or
+                grid[r][c] != "1" or 
+                (r, c) in seen
+            ):
+                return 
+
+            grid[r][c] = "0"
+            seen.add((r, c))
+
+            directions = [ [1,0], [-1, 0], [0, 1], [0, -1] ]
+            for dr, dc in directions:
+                dfs(
+                    r + dr, c + dc,
+                    seen
+                )
+
+        for i in range(ROWS):
+            for j in range(COLUMNS):
+                if grid[i][j] == "1":
+                    totalIsland+=1
+                    dfs(i, j, set())
+
+        return totalIsland
+        
+
+
+
